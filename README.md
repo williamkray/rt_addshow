46c8e3056f5c302c79c1e36d3388a4906e3a00db
William Kray <william.kray@gmail.com>
2014-12-20 14:54:55 -0800

    First commit for github. Yay!

---
+++ README.md
@@ -0,0 +1,33 @@
+DESCRIPTION:
+============
+
+This script will automatically update your rtorrent and rss application configurations to make adding a torrent rss subscription easy!
+
+SETUP:
+======
+i strongly recommend setting a secondary rtorrent config file for your shows, and adding the following line to your main rtorrent config file (assuming the secondary config is called ~/.rtorrent.shows): 
+
+import=~/.rtorrent.shows
+
+This will allow you to easily reload the config without rtorrent complaining about a bunch of stuff that only should happen at boot.
+If you have xmlrpc configured for your rtorrent installation (if you're using any sort of client for rtorrent like ruTorent or something like that, then you do), the addshow script will reload this config for you. great!
+
+copy addshow.yml to your home directory as a dot file (/home/username/.addshow.yml)
+
+edit it as indicated in the comments.
+
+put the addshow.rb script somewhere in your path so you can run it. make it executable if you don't want to call ruby to run it.
+
+USAGE:
+======
+
+you can pass it arguments for a show, or it will prompt you. To pass it arguments, you could do something like this:
+
+[user@box] $ addshow.rb "An Awesome TV Show" "http://www.tvshow-url.com/rss"
+
+or simply run the script and it will prompt you for a tv show name and RSS url. If you don't have an RSS url, you can leave it empty.
+
+This script currently only supports rssdler (https://code.google.com/p/rssdler/) with plans to support flexget in the future, but i'm not there yet.
+
+If the script has trouble reloading the rtorrent config, or your rtorrent is not configured to use xmlrpc commands, you will need to manually reload or restart rtorrent to see changes. to reload the config from the rtorrent n-curses interface, hit CTRL-X to run a command, and type the same line as the one you added to your config file. easy.
+
---
+++ addshow.rb
@@ -0,0 +1,221 @@
+#!/usr/bin/ruby
+
+require 'yaml'
+require 'xmlrpc/client'
+require 'fileutils'
+require 'pp'
+
+@show_name = ARGV[0]
+@show_url = ARGV[1]
+@addshow_config = YAML.load_file("/home/#{ENV['USER']}/.addshow.yml")
+@xmlrpc_port = nil
+@rt_config_lines = nil
+@watch = nil
+@dest = nil
+@title = nil
+
+# we'll use this to clean up show names that have problematic characters in the title
+def sanitize(messy_name)
+	return messy_name.gsub(/[ \?\*\:\-\!]/, '_')
+end
+
+# this actually creates the watch and destination directories, and if necessary applies the appropriate permissions to them
+def make_folders
+	permission_error = "####################" +
+			"WARNING:" +
+			"####################" +
+			"Looks like you're getting permission issues. You have a few options:\n" + 
+			"	a) change the ownership manually after this script runs (easiest)\n" +
+			"	b) delete these folders and re-run this script as the user you're trying to set permissions for\n" +
+			"	c) use sudo/root to run this script"
+
+	puts "Making folders"
+	FileUtils.mkdir_p(@watch_dir)
+	FileUtils.mkdir_p(@dest_dir)
+
+	if !@addshow_config['watch']['mode'].nil?
+		puts "Changing watch directory permissions"
+		FileUtils.chmod(@addshow_config['watch']['mode'], @watch_dir, :verbose => true)
+	end
+
+	if !@addshow_config['watch']['owner'].nil? || !@addshow_config['watch']['group'].nil?
+		puts "Changing watch directory ownership"
+		begin
+			FileUtils.chown(@addshow_config['watch']['owner'], @addshow_config['watch']['group'], @watch_dir, :verbose => true)
+		rescue
+			puts permission_error
+		end
+	end
+
+	if !@addshow_config['dest']['mode'].nil?
+		puts "Changing destination directory permissions"
+		FileUtils.chmod(@addshow_config['dest']['mode'], @dest_dir, :verbose => true)
+	end
+
+	if !@addshow_config['dest']['owner'].nil? || !@addshow_config['dest']['group'].nil?
+		begin
+			puts "Changing destination directory ownership"
+			FileUtils.chown(@addshow_config['dest']['owner'], @addshow_config['dest']['group'], @dest_dir, :verbose => true)
+		rescue
+			puts permission_error
+		end
+	end
+
+end
+
+# this writes out a string of text with some values in it to add to your rtorrent config file
+def get_rt_line
+	@rt_config_lines = "\n\n## #{@title}\nschedule = watch_directory_#{@digit},5,5,\"load_start=#{@watch}/*.torrent,d.set_custom1=#{@dest}/d.delete_tied=\""
+end
+
+# this appends the necessary lines to our rss app and rtorrent config files
+def mod_configs
+
+	# RSS app: skips if no url is supplied, then modifies config based on supported app.
+	# if 'none' is selected, skips process. If unrecognized app, exits without making changes.
+	if @show_url == nil or @show_url == ""
+		puts "No url specified, skipping"
+	else
+		if @addshow_config['rss']['app'] == 'none'
+			puts "No rss app selected, skipping"
+		elsif @addshow_config['rss']['app'] == 'rssdler'
+			puts "Modifying rssdler config file"
+			rssdler_config_lines = "\n\n" +
+				"[ #{@clean_name} ]\n"+
+				"link=#{@show_url}\n"+
+				"directory=#{@watch_dir}"
+			File.open(@addshow_config['rss']['config'], 'a') do |f|
+				f.write(rssdler_config_lines)
+			end
+		else
+			puts "Sorry, RSS app is unrecognized. Exiting."
+			exit
+		end
+	end
+
+	# rtorrent: checks last line of rtorrent config for previous watch directory number.
+	# If not parseable, prints message of dummy line to add to end of file manually,
+	# with option to automatically add it now.
+	# Otherwise, increments number by one and adds config line for new watch directory.
+
+	@digit = 0
+	@watch = "dummy"
+	@dest = "dummy"
+	@title = "Dummy Line"
+	
+	while @digit == 0 or @digit == nil
+		get_rt_line
+		rt_last_line = File.readlines(@addshow_config['rtorrent']['config_shows'])[-1]
+		@digit = rt_last_line.split(',').first.to_s.split(//).last(4).join.to_i unless rt_last_line !~ /watch_directory_/
+		if @digit == nil or @digit == 0
+			@digit = 1000
+			get_rt_line
+			puts "Hmm, I'm having some trouble configuring your rtorrent file.\n"+
+				"Would you like to automatically add the following dummy line to the end of it?"+
+				"#{@rt_config_lines}\n\n"+
+				"(Y[es] to add it, N[o] to exit without making changes)"
+			mod_rtorrent = nil
+			while mod_rtorrent == nil
+				mod_rtorrent = $stdin.gets.chomp!
+				if mod_rtorrent =~ /^[Yy][Ee]*[Ss]*/
+					File.open(@addshow_config['rtorrent']['config_shows'], 'a') do |f|
+						f.write(@rt_config_lines)
+					end
+					@digit = 0
+				elsif mod_rtorrent =~ /^[Nn][Oo]*$/
+					puts "Exiting"
+					exit
+				else
+					puts "I didn't catch that. Try again."
+					mod_rtorrent = nil
+				end
+			end
+		end
+	end
+
+	@digit += 1
+	@watch = @watch_dir
+	@dest = @dest_dir
+	@title = @show_name
+
+	get_rt_line
+
+	puts "Adding the following lines to the rtorrent config:"+
+		"#{@rt_config_lines}"+"\n"
+		
+	File.open(@addshow_config['rtorrent']['config_shows'], 'a') do |f|
+		f.write(@rt_config_lines)
+	end
+
+end
+
+def reload_rtorrent
+	if @xmlrpc_port == nil
+		puts "No xmlrpc port defined, skipping rtorrent reload. You can reload your config file manually, or restart rtorrent to see configuration changes."
+	else
+		conn = XMLRPC::Client.new('localhost', '/RPC2', 80)
+		begin
+			conn.call('import', '', "@addshow_config['rtorrent']['config_shows']")
+		rescue XMLRPC::FaultException => e
+			puts "Whoa, looks like your xmlrpc configuration on rtorrent is a little funny. You'll have to reload or restart rtorrent manually. The full error is below:"
+			puts e.faultString
+			exit
+		end
+	end
+end
+
+File.read(@addshow_config['rtorrent']['config_main']).each_line do |line|
+	if line =~ /^\s*scgi_port/
+		@xmlrpc_port = line.split(':').last
+	end
+end
+
+if @xmlrpc_port.nil?
+	puts "####################\n" +
+	"WARNING: rtorrent not configured for xmlrpc interface. rtorrent will be need to be MANUALLY RESTARTED/RELOADED.\n" +
+	"####################"
+end
+
+if @show_name.nil?
+	print "Name of show: "
+	@show_name = $stdin.gets.chomp
+end
+
+if @show_url.nil?
+	print "URL for RSS feed (leave blank for none): "
+	@show_url = $stdin.gets.chomp
+end
+
+@clean_name = sanitize(@show_name)
+@watch_dir = "#{@addshow_config['watch']['path']}/#{@clean_name}"
+@dest_dir = "#{@addshow_config['dest']['path']}/#{@clean_name}"
+
+puts "the following configurations are set:\n
+	RSS config file: #{@addshow_config['rss']['config']}
+	rtorrent config file: #{@addshow_config['rtorrent']['config_shows']}
+	watch directory: #{@watch_dir}
+	destination directory: #{@dest_dir}
+	Show name: #{@show_name}
+	RSS URL: #{@show_url}
+
+Does this look correct?"
+
+confirm = nil
+while confirm.nil?
+	confirm = $stdin.gets.chomp!
+	if confirm =~ /^[Yy][Ee]*[Ss]*/
+		if !File.exists?(@addshow_config['rtorrent']['config_shows'])
+			File.open(@addshow_config['rtorrent']['config_shows'], "w")
+		end
+		make_folders
+		mod_configs
+		reload_rtorrent
+	elsif confirm =~ /^[Nn][Oo]*$/
+		puts "K, never mind. Exiting."
+	else
+		puts "I didn't catch that. Try again."
+		confirm = nil
+	end
+end
+
+
